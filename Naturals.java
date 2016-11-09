216f466520ee2827f2b4d827ab462bb8a47f028b
masssuresh <sureshmass03@gmail.com>
2016-11-09 16:19:48 +0530

    Add files via upload

---
+++ Naturals.java
@@ -0,0 +1,18 @@
+class Greater
+{
+public static void main(String[]args)
+{
+int a=5,b=10,c=6;
+if(a>b)
+{
+System.out.println("a is greater than b");
+}
+elseif(a>c)
+{
+System.out.println("a is not greater than c");
+}
+else
+{
+System.out.println("a is not greater than c");
+}}
+}
